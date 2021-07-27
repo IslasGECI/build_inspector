8e19151483fe6217440e0bf8837b729da9220025
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-07-26 23:20:32 -0700

    ♻️ Extract function to its own file

--- src/geci-maketests.sh
+++ src/geci-maketests.sh
@@ -10,17 +10,7 @@ uuid=$2
 branch=develop
 image_tag=latest
 
-function notify_healthchecks {
-  curl \
-    --data-raw "$2" \
-    --fail \
-    --max-time 10 \
-    --output /dev/null \
-    --retry 5 \
-    --show-error \
-    --silent \
-    https://hc-ping.com/"$1"
-}
+source ./src/helper.sh
 
 log="tests@${repository}:${image_tag}"
 notify_healthchecks ${uuid}/start ${log}
