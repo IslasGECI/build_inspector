eec71e0f19666b801b285a412833531641e8ffe7
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-07-26 23:09:16 -0700

    🧑‍💻 Remove operator "&&"

--- src/loop.sh
+++ src/loop.sh
@@ -6,7 +6,7 @@ while IFS="," read -r repo uuid
 do
   echo "Repo: ${repo}"
   echo "UUID: ${uuid}"
-  ./src/geci-maketests.sh ${repo} ${uuid} && \
+  ./src/geci-maketests.sh ${repo} ${uuid}
   ./src/geci-makeall.sh ${repo} ${uuid}
   echo ""
 done < <(tail --lines=+2 ${DATAFILE})
