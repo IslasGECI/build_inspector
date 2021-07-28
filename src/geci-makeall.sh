2a6d98b6454b673476692242e9cb68f41b79b00a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-07-28 13:25:55 -0700

    🧑‍💻 Fail all if any fails

--- src/geci-makeall.sh
+++ src/geci-makeall.sh
@@ -16,6 +16,7 @@ cd ${repository}
 git checkout ${branch}
 git pull
 all_reports=$(jq --raw-output ".[].report" analyses.json)
+error_count=0
 i=0
 for report_name in $all_reports
 do
@@ -29,6 +30,14 @@ do
     docker pull islasgeci/${repository}:${image_tag}
     docker run --env BITBUCKET_USERNAME=${BITBUCKET_USERNAME} --env BITBUCKET_PASSWORD=${BITBUCKET_PASSWORD} --rm --volume ${PWD}:/workdir islasgeci/${repository}:${image_tag} bash -c "make clean && make reports/$report_name" \
         && notify_healthchecks ${uuid} ${log} \
-        || notify_healthchecks ${uuid}/fail ${log}
+        || ( notify_healthchecks ${uuid}/fail ${log} ; ((error_count = error_count + 1)) )
     ((i = i + 1))
 done
+
+log="geci-makeall@${repository}"
+if [ $error_count -eq 0 ]
+then
+    notify_healthchecks ${uuid} ${log}
+else
+    notify_healthchecks ${uuid}/fail ${log}
+fi
