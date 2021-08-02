410417b3235f8308aefe403986552fab49a97c32
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-02 07:58:59 -0700

    🧑‍💻 Add script for inspecting a list

---
+++ src/inspect_list.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+datafile=data/raw/repository_list.csv
+
+while IFS="," read -r repository uuid
+do
+  echo ""
+  echo "========================================"
+  echo "Repository: ${repository}"
+  echo "UUID: ${uuid}"
+  ./src/geci-maketests.sh ${repository} ${uuid}
+  ./src/geci-makeall.sh ${repository} ${uuid}
+  echo ""
+done < <(tail --lines=+2 ${datafile})
