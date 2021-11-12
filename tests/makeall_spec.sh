cb5910cf327dfb121109de6ff7ee034d412abb1c
Xavier Paredes <jxavierpp1@gmail.com>
2021-11-12 12:58:45 -0800

    ✅ Pass tests

--- tests/makeall_spec.sh
+++ tests/makeall_spec.sh
@@ -22,10 +22,10 @@ Describe 'geci-makeall.sh'
   It 'Notifies healtchecks.io'
     When call src/geci-makeall.sh repositorio 1234567890
     The lines of stdout should equal 26
-    The line 3 of output should equal "curl --data-raw reporte_1.pdf@repositorio:abcd --fail --max-time 10 --retry 5 --show-error --silent https://hc-ping.com/1234567890/start"
+    The line 3 of output should equal "curl --data-raw reporte_1.pdf@repositorio:abcd --fail --max-time 10 --output /dev/null --retry 5 --show-error --silent https://hc-ping.com/1234567890/start"
     The line 6 of output should equal "reporte_1.pdf@repositorio:abcd"
     The line 15 of output should equal "reporte_2.pdf@repositorio:wxyz"
-    The line 25 of output should equal "curl --data-raw geci-makeall@repositorio:develop --fail --max-time 10 --retry 5 --show-error --silent https://hc-ping.com/1234567890"
+    The line 25 of output should equal "curl --data-raw geci-makeall@repositorio:develop --fail --max-time 10 --output /dev/null --retry 5 --show-error --silent https://hc-ping.com/1234567890"
     The line 26 of output should equal "PASS"
   End
 End
