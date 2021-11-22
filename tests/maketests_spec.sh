a95fc4188cdbf1f7cebe06e69bf47371c938fde1
Xavier Paredes <jxavierpp1@gmail.com>
2021-11-22 12:35:28 -0800

    ♻️ Refactor removing duplicate code

--- tests/maketests_spec.sh
+++ tests/maketests_spec.sh
@@ -15,9 +15,11 @@ Describe 'geci-maketests.sh'
   End
 
   It 'Notifies healtchecks.io'
+  output_head="curl --data-raw"
+    output_tail="--fail --max-time 10 --output /dev/null --retry 5 --show-error --silent https://hc-ping.com/4fd77125-7f38-4996-b1e8-bbd337b338"
     When call src/geci-maketests.sh isla-guadalupe 4fd77125-7f38-4996-b1e8-bbd337b338
     The lines of stdout should equal 6
-    The first line of output should equal "curl --data-raw tests@isla-guadalupe:latest --fail --max-time 10 --output /dev/null --retry 5 --show-error --silent https://hc-ping.com/4fd77125-7f38-4996-b1e8-bbd337b338/start"
-    The line 6 of output should equal "curl --data-raw tests@isla-guadalupe:latest --fail --max-time 10 --output /dev/null --retry 5 --show-error --silent https://hc-ping.com/4fd77125-7f38-4996-b1e8-bbd337b338"
+    The first line of output should equal "${output_head} tests@isla-guadalupe:latest ${output_tail}/start"
+    The line 6 of output should equal "${output_head} tests@isla-guadalupe:latest ${output_tail}"
   End
 End
